624dde9b2ea1fa1fcfbc5141643d77d337629d9e
Anand <anand.simplextech@gmail.com>
2026-07-23 11:11:19 +0530

    refactor(Package): updated package version

--- Sources/NSFeatureFishMock/Presentation/View/FishLandingView.swift
+++ Sources/NSFeatureFishMock/Presentation/View/FishLandingView.swift
@@ -14,7 +14,61 @@ struct FishLandingView: View {
     var fishViewModel: FishViewModel
     
     var body: some View {
-        Text("Featur Fish")
+        List {
+            ReusableHeaderView(style: .standard(title: "Shop by category", subtitle: nil))
+            NAVerticalGrid_NxN_V2()
+            ReusableHeaderView(style: .standard(title: "From Marine/Sea", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "From Freshwater/Lake", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Crab", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Prawns/Shell Fish", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Exotic", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Boneless", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Steaks", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Dry Fish", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Freshly Frozen", subtitle: nil))
+            NAHorizontalGrid_NxN_V2()
+            SeeAllButtonViewV2() {
+                
+            }
+        }
+        .listStyle(.plain)
+        .listRowSpacing(0)
     }
 }
 
+// MARK: - Preview Setup Engine
+#Preview {
+    FishLandingView(fishViewModel: FishViewModel())
+}
